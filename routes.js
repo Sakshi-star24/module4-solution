59d763ff3de507d025965de122f482880d851919
Sakshi-star24 <63860914+Sakshi-star24@users.noreply.github.com>
2020-09-13 14:43:24 +0530

    Update routes.js

--- routes.js
+++ routes.js
@@ -15,13 +15,13 @@ function RoutesConfig($stateProvider, $urlRouterProvider) {
 
   // Home page
   .state('home', {
-    url: '/',
+    url: '',
     templateUrl: 'home.template.html'
   })
 
   // Categories
   .state('mainList', {
-    url: '/main-list',
+    url: 'main-list',
     templateUrl: 'main-categories.template.html',
     controller: 'MainMenuAppController as mainList',
     resolve: {
@@ -33,7 +33,7 @@ function RoutesConfig($stateProvider, $urlRouterProvider) {
 
 
    .state('itemDetail', {
-    url: '/item-detail/{itemId}',
+    url: 'item-detail/{itemId}',
     templateUrl: 'item-detail.template.html',
     controller: 'ItemDetailController as itemDetail',
     resolve: {
